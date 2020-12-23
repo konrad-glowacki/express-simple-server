fe6effc5d29090a7983bf9e4a12c24c7e591f76f
Adam Sowiński <adassow@gmail.com>
2020-12-23 09:27:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 server.get('/', (req, res) => {
   res.send('Hello Express2!');
+  for (let i = 0; i < 500; i++) {
+    console.log(`Foooo`);
+  }
 });
 
 server.listen(PORT, () => {
