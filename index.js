1eeff0973e5752b0e3364bc2591a5e2ceedce4c0
Adam Sowiński <adam.sowinski@wpengine.com>
2022-10-12 13:50:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.get('/', (req, res) => {
-  res.send('Hello Express18!');
+  res.send('Hello Express19!');
 });
 
 server.listen(PORT, () => {
