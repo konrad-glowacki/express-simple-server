e851ca65049a50d5e304eb83e5809f9ca7662110
Konrad Glowacki <konrad.glowacki@wpengine.com>
2025-02-07 17:07:03 +0100

    Add crash on build

--- build.js
+++ build.js
@@ -1,2 +1,7 @@
 console.log('MY_SECRET: ', process.env.MY_SECRET);
 console.log('ALL ENVS: ', process.env);
+
+if (process.env.APP_CRASH === 'true') {
+  console.log('App crashed');
+  process.exit(1);
+}
