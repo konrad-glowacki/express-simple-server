9cff8864ab568ac573600da534664970e29e0094
Konrad Głowacki <konrad.glowacki@wpengine.com>
2025-03-27 11:56:15 +0100

    Update index.js 2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get('/', (_req, res) => {
   }
 
   console.log('Hello home root message');
-  res.send('Hello Express!!!');
+  res.send('Hello Express!!!2');
 });
 
 server.get('/fetch-wordpress-graphql', async (_req, res) => {
